85939f5182ef1c1d90041feb7cf25781616ef8c9
Tornike Nanobashvili <tnanobashvili92@gmail.com>
2020-04-10 16:46:12 +0300

    Creates trade entity, its migration and sets up relationship
    	Trade refers to User, e.i a user can have many trades and
    	trade belongs to an user.

--- www/paxful/app/User.php
+++ www/paxful/app/User.php
@@ -38,4 +38,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function trades()
+    {
+        return $this->hasMany(Trade::class);
+    }
 }
---
+++ www/paxful/database/migrations/2020_04_10_125703_create_trades_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTradesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trades', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('payment_method');
+            $table->unsignedBigInteger('cents');
+            $table->string('currency');
+            $table->unsignedBigInteger('satoshis');
+            $table->string('hash');
+            $table->boolean('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trades');
+    }
+}
