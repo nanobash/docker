68489b44b46765d6f63e6929a1b708de6ab7128a
Tornike Nanobashvili <tnanobashvili92@gmail.com>
2020-04-11 17:02:43 +0300

    Reduces "Satoshi Nakamoto"'s trades to 2 in seeds

--- www/paxful/database/seeds/UserSeeder.php
+++ www/paxful/database/seeds/UserSeeder.php
@@ -16,14 +16,14 @@ class UserSeeder extends Seeder
     public function run()
     {
         // Generate random multiple users with their own trades
-        factory(User::class, 15)
+        factory(User::class, 18)
             ->create()
             ->each(function ($user) {
                 $user->trades()->save(factory(Trade::class)->make());
             });
 
         // Just for fun creating user "Satoshi Nakamoto" and assign several trades to him
-        factory(Trade::class, 5)->create([
+        factory(Trade::class, 2)->create([
             'user_id' => factory(User::class, 1)->create([
                 'username' => 'Nakamoto',
             ])->first(),
